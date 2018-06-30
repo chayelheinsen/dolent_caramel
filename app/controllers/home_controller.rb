9a900c343cb8e0259fa22f38d1c2a309593abe02
Chayel Heinsen <chayel@chayelheinsen.com>
2018-06-30 17:17:29 -0400

    Update some styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@
 
 class HomeController < ApplicationController
   def index; end
+
+  def contact
+
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :home, only: %i[index]
+  post "/contact", to: "home#contact"
 end
